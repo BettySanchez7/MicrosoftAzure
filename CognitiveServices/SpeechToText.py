26f27700ad94cf5320bd0be0d2caa8231d0f396a
BettySanchez7 <bety.sanchez.diaz@gmail.com>
2020-10-16 15:31:26 -0500

    Speech to text with azure

---
+++ CognitiveServices/SpeechToText.py
@@ -0,0 +1,24 @@
+from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer, SpeechSynthesisOutputFormat, SpeechRecognizer
+from azure.cognitiveservices.speech.audio import AudioOutputConfig
+import azure.cognitiveservices.speech as speechsdk
+
+speech_key, service_region = "Clave", "Region"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+audio_input = speechsdk.AudioConfig(filename="/content/narration.wav")
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+print("Recognizing", speech_recognizer)
+
+result = speech_recognizer.recognize_once();
+print(result)
+    # Check the result
+if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+    print("Recognized: {}".format(result.text))
+elif result.reason == speechsdk.ResultReason.NoMatch:
+    print("No speech could be recognized")
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        print("Error details: {}".format(cancellation_details.error_details))
+    # </SpeechRecognitionWithMicrophone>
